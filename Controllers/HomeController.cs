e914af7a3bcb6c3ce5bf7dd888b741eef62a8411
abhishek-cell <58728844+abhishek-cell@users.noreply.github.com>
2019-12-12 16:57:03 +0530

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace KoolApplicationMain.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("ProductDetail");
         }
         public IActionResult ProductDetail()
         {
