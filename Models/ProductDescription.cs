ceb73fada83d4a35695b4ac995db596696869017
abhishek-cell <58728844+abhishek-cell@users.noreply.github.com>
2019-12-13 17:57:43 +0530

    Add files via upload

---
+++ Models/ProductDescription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoolApplicationMain.Models
+{
+    public class ProductDescription
+    {
+        public int ItemNumber { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Brand { get; set; }
+
+    }
+}
