6874ba3f3ffcc4d61f92341d1377965381fa8089
abhishek-cell <58728844+abhishek-cell@users.noreply.github.com>
2019-12-13 17:49:31 +0530

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -44,27 +44,54 @@ namespace KoolApplicationMain.Controllers
 
             //return View();
         }
-        public IActionResult ProductDetail()
+        public IActionResult ProductDetail(int p)
         {
             Search search = new Search();
-            var model = new List<Product>();
+            var model = new List<ProductDescription>();
+            DataTable dt = new DataTable();
             using (MySqlConnection conn = search.GetConnection())
             {
                 conn.Open();
-                MySqlCommand cmd = new MySqlCommand("select XXIBM_PRODUCT_SKU.Item_number,XXIBM_PRODUCT_SKU.description,XXIBM_PRODUCT_PRICING.List_price,XXIBM_PRODUCT_PRICING.In_stock from XXIBM_PRODUCT_SKU JOIN XXIBM_PRODUCT_PRICING ON XXIBM_PRODUCT_SKU.Item_number=XXIBM_PRODUCT_PRICING.Item_number ", conn);
-                using (var reader = cmd.ExecuteReader())
+                MySqlCommand cmd = new MySqlCommand("select XXIBM_PRODUCT_SKU.Item_number,XXIBM_PRODUCT_SKU.description,XXIBM_PRODUCT_PRICING.List_price,XXIBM_PRODUCT_PRICING.In_stock from XXIBM_PRODUCT_SKU JOIN XXIBM_PRODUCT_PRICING ON XXIBM_PRODUCT_SKU.Item_number=XXIBM_PRODUCT_PRICING.Item_number where XXIBM_PRODUCT_SKU.Item_number="+p+" ", conn);
+                for (int i = 0; i < dt.Rows.Count; i++)
                 {
-                    while (reader.Read())
+                    model.Add(new ProductDescription()
                     {
-                        model.Add(new Product()
-                        {
-                            ItemNumber = Convert.ToInt32(reader["Item_number"]),
-                            Description = reader["description"].ToString(),
-                            Price = Convert.ToDouble(reader["List_price"]),
-                            Stock = reader["In_stock"].ToString()
+                        ItemNumber = Convert.ToInt32(dt.Rows[i]["Item_number"]),
+                        Description = dt.Rows[i]["description"].ToString(),
+                        Price = Convert.ToDouble(dt.Rows[i]["List_price"]),
+                        Brand = dt.Rows[i]["Brand"].ToString()
 
-                        });
-                    }
+                    });
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult EachProductDetails( )
+        {
+            Search search = new Search();
+            var model = new List<ProductDescription>();
+            using (MySqlConnection conn = search.GetConnection())
+            {
+                conn.Open();
+                DataTable dt = new DataTable();
+                string str = "select XXIBM_PRODUCT_STYLE.Brand,XXIBM_PRODUCT_SKU.Item_number,XXIBM_PRODUCT_SKU.description,XXIBM_PRODUCT_PRICING.List_price,XXIBM_PRODUCT_PRICING.In_stock from XXIBM_PRODUCT_SKU JOIN XXIBM_PRODUCT_PRICING ON XXIBM_PRODUCT_SKU.Item_number=XXIBM_PRODUCT_PRICING.Item_number JOIN XXIBM_PRODUCT_STYLE ON XXIBM_PRODUCT_STYLE.Item_number= XXIBM_PRODUCT_SKU.Item_number";
+                //MySqlCommand cmd = new MySqlCommand("select XXIBM_PRODUCT_SKU.Item_number,XXIBM_PRODUCT_SKU.description,XXIBM_PRODUCT_PRICING.List_price,XXIBM_PRODUCT_PRICING.In_stock from XXIBM_PRODUCT_SKU JOIN XXIBM_PRODUCT_PRICING ON XXIBM_PRODUCT_SKU.Item_number=XXIBM_PRODUCT_PRICING.Item_number ", conn);
+                MySqlDataAdapter mda = new MySqlDataAdapter(str, search.GetConnection());
+                mda.Fill(dt);
+                mda = new MySqlDataAdapter(str, search.GetConnection());
+                mda.Fill(dt);
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    model.Add(new ProductDescription()
+                    {
+                        ItemNumber = Convert.ToInt32(dt.Rows[i]["Item_number"]),
+                        Description = dt.Rows[i]["description"].ToString(),
+                        Price = Convert.ToDouble(dt.Rows[i]["List_price"]),
+                        Brand = dt.Rows[i]["Brand"].ToString()
+
+                    });
                 }
             }
             return View(model);
